13b63996452506cce93b764e7ab15fad76464859
mhebd <mhemonhossain@gmail.com>
2025-04-13 00:48:23 +0600

    file not found issue fixed

--- utility/index.js
+++ utility/index.js
@@ -1,6 +1,5 @@
 const asyncHdl = require('./asyncHdl');
 const errMsg = require('./errMsg');
 const Result = require('./result');
-const CRUD = require('./crudOparetion');
 
-module.exports = { asyncHdl, errMsg, Result, CRUD };
+module.exports = { asyncHdl, errMsg, Result };
